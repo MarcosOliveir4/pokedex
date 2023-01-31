20c81b990f9010b5fbf2dc911ad3244435e2aed9
MarcosOliveir4 <marcosassis071@gmail.com>
2023-01-31 10:23:08 -0300

    feat: refatorando logica para chamada da api e criacao do component de loading

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PokecardModule } from './pokecard/pokecard.module';
+import { PokeloadingModule } from './pokeloading/pokeloading.module';
 
 @NgModule({
-  imports: [CommonModule, PokecardModule],
-  exports: [PokecardModule],
+  imports: [CommonModule, PokecardModule, PokeloadingModule],
+  exports: [PokecardModule, PokeloadingModule],
 })
 export class ComponentModule {}
---
+++ src/app/components/pokeloading/pokeloading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeloading',
+  templateUrl: 'pokeloading.component.html',
+  styleUrls: ['pokeloading.component.scss'],
+})
+export class pokeloadingComponent {}
